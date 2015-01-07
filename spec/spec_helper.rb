789421b91164df90c1f919d4f8a2d030a101c528
Wolfgang Barth <wob@swobspace.net>
2015-01-07 18:13:06 +0100

    remote old config entry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ Dotenv.load( File.expand_path(__FILE__ + '/../.localenv'),
               File.expand_path(__FILE__ + '/../.env'))
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   # config.filter_run :focus
   config.mock_framework = :rspec
